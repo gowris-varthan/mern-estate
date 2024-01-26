18e29e164ca71f2229ae66ac693ee15c9554eac3
Krish test <itbncoe@gmail.com>
2024-01-26 13:34:08 +0530

    Profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile"
 import About from "./pages/About"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<Signin />}></Route>
         <Route path="/sign-out" element={<SignOut />}></Route>
+        <Route  element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />}></Route>
+        </Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
 
